a1131d1f247420b534c3c6216f3eb040aa7e0db0
Maria Angeles <maria.d.juri@gmail.com>
2023-10-19 21:09:01 -0400

    search bar works and clears

--- main.js
+++ main.js
@@ -6,32 +6,40 @@
     </div> */
 
 const musicContainer = document.querySelector('#music-container')
-let search = document.querySelector('#search')
+let searchForm = document.querySelector('#searchForm')
 let searchButton = document.querySelector('#searchButton')
 let musicSearch = document.querySelector('#music-search')
-// let url = ' https://itunes.apple.com/search?term=jack+johnson&limit=25'
 
-// search.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   console.log(event.target);
-  // fetch(url)
-  fetch('https://itunes.apple.com/search?term=bobs+burgers&entity=song')
+
+
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  clearSearchResults()
+  const baseURL = "https://itunes.apple.com/search?"
+  let searchPath = musicSearch.value
+  let finalURL = `${baseURL}term=${searchPath}&entity=song`
+  console.log(`this is the musicSearch value: ${musicSearch.value}`);
+  console.log(finalURL)
+  fetch(finalURL)
+  // fetch('https://itunes.apple.com/search?term=bobs+burgers&entity=song')
   .then((response) => {
+    // console.log(response)
     return response.json()
+
   }).then((data) => {
     // console.log(data)
     // console.log(data.results)
     // console.log(data.results[0].kind)
     buildMusic(data.results)
   })
-// })
+})
  
   // fetch('https://itunes.apple.com/search?term=bobs+burgers&entity=song') --this was on the line that currently has fetch
 
 function buildMusic(musicArray) {
-    // cd 
     for (let song of musicArray) {
-        console.log(song)
+        // console.log(song)
         let musicCard = document.createElement('div')
         musicCard.classList.add("music-cards")
         let musicImage = document.createElement('img')
@@ -49,4 +57,9 @@ function buildMusic(musicArray) {
         bandName.innerText = `${song.collectionArtistName}`
         musicCard.appendChild(bandName)
       }
+}
+
+function clearSearchResults() {
+  console.log(`it worked, bitch`)
+  musicContainer.innerHTML = []
 }
