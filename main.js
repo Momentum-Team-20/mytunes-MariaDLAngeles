a645c6b7479e097df383395936a7185356e5800e
Maria Angeles <maria.d.juri@gmail.com>
2023-10-20 10:57:22 -0400

    audio preview is on page but needs tweaking

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const musicContainer = document.querySelector('#music-container')
 let searchForm = document.querySelector('#searchForm')
 let searchButton = document.querySelector('#searchButton')
 let musicSearch = document.querySelector('#music-search')
-
-
+let audioSection = document.querySelector('audio-section')
+let audioPlayer = document.getElementById('audioPlayer')
 
 
 searchForm.addEventListener('submit', (event) => {
@@ -22,11 +22,10 @@ searchForm.addEventListener('submit', (event) => {
   console.log(`this is the musicSearch value: ${musicSearch.value}`);
   console.log(finalURL)
   fetch(finalURL)
-  // fetch('https://itunes.apple.com/search?term=bobs+burgers&entity=song')
   .then((response) => {
     // console.log(response)
     return response.json()
-
+    
   }).then((data) => {
     // console.log(data)
     // console.log(data.results)
@@ -34,32 +33,43 @@ searchForm.addEventListener('submit', (event) => {
     buildMusic(data.results)
   })
 })
- 
-  // fetch('https://itunes.apple.com/search?term=bobs+burgers&entity=song') --this was on the line that currently has fetch
+
 
 function buildMusic(musicArray) {
-    for (let song of musicArray) {
-        // console.log(song)
-        let musicCard = document.createElement('div')
-        musicCard.classList.add("music-cards")
-        let musicImage = document.createElement('img')
-        musicImage.src = song.artworkUrl100
-        musicImage.alt = 'song-image'
-        musicCard.appendChild(musicImage)
-        console.log(musicCard)
-        musicContainer.appendChild(musicCard)
-        let songTitle = document.createElement('div')
-        songTitle.classList.add("song-title")
-        songTitle.innerText = `${song.trackName}`
-        musicCard.appendChild(songTitle)
-        let bandName = document.createElement('div')
-        bandName.classList.add("band-name")
-        bandName.innerText = `${song.collectionArtistName}`
-        musicCard.appendChild(bandName)
-      }
+  for (let song of musicArray) {
+
+    let musicCard = document.createElement('div')
+    musicCard.classList.add("music-cards")
+    
+    let musicImage = document.createElement('img')
+    musicImage.src = song.artworkUrl100
+    musicImage.alt = 'song-image'
+    musicCard.appendChild(musicImage)
+    
+    musicContainer.appendChild(musicCard)
+    
+    let songTitle = document.createElement('div')
+    songTitle.classList.add("song-title")
+    songTitle.innerText = `${song.trackName}`
+    musicCard.appendChild(songTitle)
+    
+    let bandName = document.createElement('div')
+    bandName.classList.add("band-name")
+    bandName.innerText = `${song.artistName}`
+    musicCard.appendChild(bandName)
+    
+    let playButton = document.createElement('button')
+    playButton.classList.add('playButton')
+    playButton.innerText = 'Play Preview'
+    musicCard.appendChild(playButton)
+    
+    playButton.addEventListener('click', () => {
+    audioPlayer.src = song.previewURL
+    })
+  }
 }
 
 function clearSearchResults() {
-  console.log(`it worked, bitch`)
+  // console.log(`it worked!`)
   musicContainer.innerHTML = []
 }
